c00b98024af243c09afdd09488d00148356620c1
agc11 <adrian@smartenergyassets.com>
2021-07-25 18:24:01 +0200

    get paginate symbols

---
+++ netnet/ExtractorSymbol.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+from typing import List
+import requests
+import re
+import json
+
+COMMON_HEADER = {'Connection': 'keep-alive',
+                  'Expires': '-1',
+                  'Upgrade-Insecure-Requests': '1',
+                  'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0",
+                  }
+class ExtractorSymbol:
+    def __init__(self, countries: List[str]):
+        self.countries = countries
+        self._prepare()
+
+    def _prepare(self) -> ExtractorSymbol:
+        website = requests.get(
+            "https://finance.yahoo.com/screener/new/", headers=COMMON_HEADER)
+        self.crumb = re.findall(
+            '"CrumbStore":{"crumb":"(.+?)"}', str(website.content))[0]
+        self.cookies = website.cookies
+
+    def get_data(self):
+        countries_filter = [{
+                            "operator": "EQ",
+                            "operands": [
+                                "region",
+                                c
+                            ]
+                            } for c in self.countries]
+        body = {
+            "size": 200,
+            "offset": 0,
+            "sortField": "intradaymarketcap",
+            "sortType": "DESC",
+            "quoteType": "EQUITY",
+            "topOperator": "AND",
+            "query": {
+                "operator": "AND",
+                "operands": [
+                    {
+                        "operator": "or",
+                        "operands": countries_filter
+                    },
+                    {
+                        "operator": "or",
+                        "operands": [
+                            {
+                                "operator": "LT",
+                                "operands": [
+                                    "intradaymarketcap",
+                                    2000000000
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "operator": "lt",
+                        "operands": [
+                            "lastclosepricebookvalue.lasttwelvemonths",
+                            1
+                        ]
+                    }
+                ]
+            }
+        }
+        quotes = []
+        end = False
+        while not end:
+            response = requests.post(f"https://query2.finance.yahoo.com/v1/finance/screener?crumb={self.crumb}&lang=en-US&region=US"
+                                 f"&formatted=true&corsDomain=finance.yahoo.com", cookies=self.cookies, data=json.dumps(body), headers=COMMON_HEADER)
+            data = json.loads(response.content).get('finance').get('result')[0]
+            if body['offset'] > data['total']:
+                end = True
+            body['offset'] += body['size']
+
+            quotes.extend(data['quotes'])
+        return quotes
